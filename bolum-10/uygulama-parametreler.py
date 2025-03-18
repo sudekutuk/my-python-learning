3067e2489a611e816f3e63def7a8181412f53d82
Sude Naz Kütük <115663635+sudekutuk@users.noreply.github.com>
2025-03-18 05:56:02 +0300

    gerekli fonksiyonlar eklenir

--- bolum-10/uygulama-parametreler.py
+++ bolum-10/uygulama-parametreler.py
@@ -19,11 +19,59 @@ hesapBilgileri = [
         "bakiye": 10000,
         "ekHesap": 1000,
         "username": "emremre",
-        "password": "12345q"
+        "password": "12345"
     }
 ]
 def menu(hesap):
+    print("\n")
+
+    print(f"merhaba, {hesap["ad"]} ")
+
+    print("1- Bakiye Sorgulama")
+    print("2- Para Cekme")
+    print("3- Para Yatirma")
+
+    islem = input("Yapmak Istediginiz Islem: ")
+
+    if islem == "1":
+        bakiyeSorgulama(hesap)
+    elif islem == "2":
+        paraCekme(hesap)
+    elif islem == "3":
+        paraYatirma(hesap)
+    else:
+        print("Lutfen tekrar secim yapiniz.")
+
+    menu(hesap)
+
+def paraYatirma(hesap):
     pass
+
+def paraCekme(hesap):
+    miktar = float(input("cekmek istediginiz miktar: "))
+    if hesap["bakiye"] >= miktar:
+        hesap["bakiye"] -= miktar
+        print("paranizi alabilirsiniz.")
+    else:
+        toplam = hesap["bakiye"] + hesap["ekHesap"]
+
+        if toplam >= miktar:
+            ekHesapKullanimIzni = input("ek hesap kullanmak ister misiniz?")
+            
+            if ekHesapKullanimIzni == "e":
+                kullanilacakMiktar = miktar - hesap["bakiye"]
+                hesap["bakiye"] = 0
+                hesap["ekHesap"] -= kullanilacakMiktar
+                print("paranizi alabilirsiniz")
+            else:
+                print("bakiye yetersiz...")
+        else:
+                print("bakiye yetersiz...")
+
+def bakiyeSorgulama(hesap):
+    print(f"bakiye: {hesap["bakiye"]}")
+    print(f"ek bakiye: {hesap["ekHesap"]}")
+
 
 def login():
     username = input("username: ")

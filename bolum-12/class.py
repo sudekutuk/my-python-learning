ff3b1aabcfd71a0878d4437b2368a115f53abf7b
Sude Naz Kütük <115663635+sudekutuk@users.noreply.github.com>
2025-03-21 14:55:57 +0300

    Class yapisi incelendi.

---
+++ bolum-12/class.py
@@ -0,0 +1,18 @@
+# class
+class Product:
+    pass
+    # method
+    # attribute, property
+
+# Instance , Nesne
+
+p1 = Product()
+p2 = Product()
+
+sonuc = type(p1)
+sonuc = type(p2)
+sonuc = p1
+sonuc = p2
+
+print(sonuc)
+

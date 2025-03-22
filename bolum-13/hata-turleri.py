f9874a3fb20dc8e342122e02f66c3da0d08f4f22
Sude Naz Kütük <115663635+sudekutuk@users.noreply.github.com>
2025-03-22 07:17:13 +0300

    Hata ve turleri incelenir.

---
+++ bolum-13/hata-turleri.py
@@ -0,0 +1,25 @@
+
+# ZeroDivisionError
+# ValueError
+# SyntaxError => yazim yanlisi
+
+# NameError => tanimlanmamis bir degisken kullanimi
+# print(ad)
+
+# TypeError => hatali parametre kullanimi
+# len(10)
+# 10 + "a"
+
+# IndexError => yanlis index kullanimi
+# liste = [10]
+# liste[1]
+
+# KeyError => key hatasi
+# a = {}
+# a["ad"]
+
+# AttributeError => olmayan bir ozellige ulasmak istedigimizde
+# "merhaba".Upper()
+# "merhaba".upper()
+
+### Exception Objeleri (Built-in Exception)

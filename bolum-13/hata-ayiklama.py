b54ccff351107997333315bcdf0975e38cfbfa0a
Sude Naz Kütük <115663635+sudekutuk@users.noreply.github.com>
2025-03-23 14:40:54 +0300

    Hata ayiklama nasil yapilir incelenir.

---
+++ bolum-13/hata-ayiklama.py
@@ -0,0 +1,27 @@
+# import pdb
+
+# one = "one"
+# two = "two"
+
+# pdb.set_trace()
+
+# sonuc = one + two
+
+# three = "three"
+
+# sonuc += three
+
+# print(sonuc)
+
+def toplama(a,b,c):
+    import pdb; pdb.set_trace()
+    return a + b + c
+
+toplama(10,20,30)
+
+# l => listelemen
+
+# n => next line
+# p => print
+# c => continue
+

fc2ac32a0bfde62ed324933a3df3d3746fa2595d
Sude Naz Kütük <115663635+sudekutuk@users.noreply.github.com>
2025-03-23 18:41:46 +0300

    Standart module ve harici paketler incelenir.

---
+++ bolum-14/harici-paket.py
@@ -0,0 +1,3 @@
+from termcolor import colored # type: ignore
+
+sonuc = colored("merhaba",color="green", on_color = "on_blue")
---
+++ bolum-14/standart-module.py
@@ -0,0 +1,9 @@
+import math
+
+sonuc = math.sqrt(9)
+sonuc = math.factorial(5)
+sonuc = math.floor(5)
+sonuc = math.ceil(5.9)
+print(sonuc)
+
+# random, datetime

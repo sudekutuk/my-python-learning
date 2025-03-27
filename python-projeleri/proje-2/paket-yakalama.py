48e523c0e89d07e53a57b6949b7a64e6142d3988
Sude Naz Kütük <115663635+sudekutuk@users.noreply.github.com>
2025-03-27 23:40:05 +0300

    Kod: Scapy ile ag paketleri dinlenir ve olasi saldiri paket sayisina gore tespit edilir.

--- python-projeleri/proje-2/paket-yakalama.py
+++ python-projeleri/proje-2/paket-yakalama.py
@@ -1,2 +1,26 @@
 from scapy.all import sniff
+import time
 
+sure = 5  
+sinir_degeri = 100
+paket_sayisi = 0
+starting_time = time.time()
+
+def paketleri_yakala(paket):
+    global paket_sayisi, starting_time
+
+   
+    current_time = time.time()
+    if current_time - starting_time > sure:
+     
+        print(f"{sure} saniyede gelen paket sayısı: {paket_sayisi}")
+        if paket_sayisi > sinir_degeri:
+            print("Olası saldırı tespit edildi!")
+        paket_sayisi = 0
+        starting_time = current_time
+
+   
+    paket_sayisi += 1
+
+print("Ağ trafiği izleniyor... (Çıkmak için CTRL+C)")
+sniff(prn=paketleri_yakala)
